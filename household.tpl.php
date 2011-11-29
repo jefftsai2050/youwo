89c5687a0002ad3fe517865d46a44d1c2051d262
Jeff Cai <jeff.cai.ming@gmail.com>
2011-11-29 18:33:42 +0800

    Show the table data in a page (youwo_household_info)

--- household.tpl.php
+++ household.tpl.php
@@ -2,5 +2,6 @@
 	<ul>
 		<li><a href="?q=household_top10" target="_parent">北京十大家政公司</a></li>
 		<li><a href="?q=household_howto" target="_top">如何选择家政公司</a></li>
+		<li><a href="?q=how-to-find-a-good-babysitter" target="_top">如何找个好保姆</a></li>
 	</ul>
 </div>
