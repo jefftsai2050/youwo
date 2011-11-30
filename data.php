a3c3d0b29c8cc8046967da02f180dba4bb23511b
Jeff Cai <jefftsai2050@hotmail.com>
2011-12-01 00:18:07 +0800

    Add the install data function

--- data.php
+++ data.php
@@ -5,6 +5,9 @@ function insert_youwo_company() {
 		array('company_name' => '北京市爱侬家政服务有限责任公司',
 		      'company_brief_name' => '爱侬家政',
 			  'tid' => 0),
+		array('company_name' => '北京市泰维峰家政公司',
+		      'company_brief_name' => '泰维峰家政',
+			  'tid' => 0),
 	    );
 
 	foreach ($company_info as $row) {
@@ -16,5 +19,11 @@ function insert_youwo_company() {
 			 ))
 		   ->execute();
     }
+
+	return;
 }
 
+function youwo_install_data() {
+	insert_youwo_company();
+	return "";
+}
