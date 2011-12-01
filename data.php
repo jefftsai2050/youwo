f8b8a85aa531b060ce9074c64459e8b61a7fc981
Jeff Cai <jefftsai2050@hotmail.com>
2011-12-01 23:11:23 +0800

    Add some init data code.

--- data.php
+++ data.php
@@ -23,7 +23,53 @@ function insert_youwo_company() {
 	return;
 }
 
+function insert_household_basic()
+{
+	$household_info = array(
+		array( 'company_id' => 1,
+		       'is_group' => 1, 
+			   'subgroup_cnt' => 50,
+			   'have_website' => 1,
+			   'website_addr' => 'www.ainong.cn',
+			   'service_type' => 1,
+			 ),
+		array( 'company_id' => 2,
+		       'is_group' => 1, 
+			   'subgroup_cnt' => 10,
+			   'have_website' => 1,
+			   'website_addr' => 'www.taiweifeng.com',
+			   'service_type' => 1,
+			 ),
+	);
+
+	foreach ($household_info as $row) {
+		$id = db_insert('youwo_household_basic')
+		   ->fields(array(
+		     'company_id' => $row['company_id'],
+		     'is_group' => $row['is_group'],
+		     'subgroup_cnt' => $row['subgroup_cnt'],
+		     'have_website' => $row['have_website'],
+		     'website_addr' => $row['website_addr'],
+		     'service_type' => $row['service_type'],
+			 ))
+		   ->execute();
+	}
+	return;
+}
+
+function remove_data() {
+   $num_deleted = db_delete('youwo_household_basic')
+       ->execute();
+
+   $num_deleted = db_delete('youwo_company')
+       ->execute();
+}
+	
+
+
 function youwo_install_data() {
+	remove_data();
 	insert_youwo_company();
+	insert_household_basic();
 	return "";
 }
