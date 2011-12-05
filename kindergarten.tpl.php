3692b932eec59b04c539d67cd561c3782d872a05
Jeff Cai <jefftsai2050@hotmail.com>
2011-12-05 23:40:51 +0800

    Add a class kindergarten

---
+++ kindergarten.tpl.php
@@ -0,0 +1,11 @@
+<div id="page">
+	<ul>
+		<li><a href="?q=household_top10" target="_parent">北京十大幼儿园</a></li>
+		<li><a href="?q=kindergarten_howto" target="_top">如何选择幼儿园</a></li>
+	</ul>
+</div>
+
+<div>
+<p>找幼儿园是不是让您烦恼呀？如果您工作比较繁忙，没有时间仔细考察您居住周围的幼儿园，那么您可以把这个工作委托给我们，我们会根据要求尽力帮助您找到一个好的幼儿园。</p>
+
+<p>下面，就请您<a href='?q=youwo/create_request'>给我们发送任务请求吧</a>!</p>
